27478b661f8de7f7e13a121a993e2a4c843c1e29
hirotoshi kawabuchi <hirotoshi.kawabuchi@gmail.com>
2018-09-24 19:33:22 +0900

    mailerを解除

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Rails.application.routes.draw do
     resources :likes, only: [:create, :destroy]
   end
 
+
   resources :relationships,only: [:create, :destroy]
 
   #mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
